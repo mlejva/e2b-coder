ffae0be6d2c366b6156f543ffbfa4ae20f5c6dc4
mlejva <vasek.mlejnsky@gmail.com>
2025-07-05 02:23:47 +0000

    Add basic Space Invaders game with HTML and JavaScript

---
+++ game.js
@@ -0,0 +1,92 @@
+// Space Invaders Game in JavaScript
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const player = {
+    x: canvas.width / 2 - 20,
+    y: canvas.height - 60,
+    width: 40,
+    height: 20,
+    color: 'green',
+    speed: 5
+};
+
+const bullets = [];
+
+const enemies = [];
+const enemyRowCount = 5;
+const enemyColumnCount = 10;
+const enemyWidth = 40;
+const enemyHeight = 20;
+const enemyPadding = 10;
+const enemyOffsetTop = 30;
+const enemyOffsetLeft = 30;
+
+// Create enemies
+for(let c = 0; c < enemyColumnCount; c++) {
+    enemies[c] = [];
+    for(let r = 0; r < enemyRowCount; r++) {
+        const enemyX = c * (enemyWidth + enemyPadding) + enemyOffsetLeft;
+        const enemyY = r * (enemyHeight + enemyPadding) + enemyOffsetTop;
+        enemies[c][r] = { x: enemyX, y: enemyY, status: 1 };
+    }
+}
+
+// Game loop
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw player
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    // Draw bullets
+    bullets.forEach((bullet, index) => {
+        if (bullet.y + bullet.height < 0) {
+            bullets.splice(index, 1);
+        } else {
+            bullet.y -= bullet.speed;
+            ctx.fillStyle = bullet.color;
+            ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+        }
+    });
+
+    // Draw enemies
+    for(let c = 0; c < enemyColumnCount; c++) {
+        for(let r = 0; r < enemyRowCount; r++) {
+            if (enemies[c][r].status == 1) {
+                const enemyX = enemies[c][r].x;
+                const enemyY = enemies[c][r].y;
+                ctx.fillStyle = 'red';
+                ctx.fillRect(enemyX, enemyY, enemyWidth, enemyHeight);
+            }
+        }
+    }
+
+    requestAnimationFrame(draw);
+}
+
+draw();
+
+// Event listeners for player movement and shooting
+window.addEventListener('keydown', function(event) {
+    switch(event.key) {
+        case 'ArrowLeft':
+            if (player.x > 0) player.x -= player.speed;
+            break;
+        case 'ArrowRight':
+            if (player.x + player.width < canvas.width) player.x += player.speed;
+            break;
+        case ' ': // Space bar
+            bullets.push({
+                x: player.x + player.width / 2 - 2,
+                y: player.y,
+                width: 4,
+                height: 10,
+                color: 'yellow',
+                speed: 7
+            });
+            break;
+    }
+});
